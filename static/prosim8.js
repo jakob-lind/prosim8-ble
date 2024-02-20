17399993a1a089e6036546688f5bb61e67acf036
Jakob Lind <jakob.lind@raysafe.com>
2024-02-20 23:01:36 +0100

    Trend functionality is now working, although missing some features.

--- static/prosim8.js
+++ static/prosim8.js
@@ -1,8 +1,50 @@
 const serviceUuid = "00000001-710e-4a5b-8d75-3e5b444bc3cf";
-const characteristicUuid = "00000003-710e-4a5b-8d75-3e5b444bc3cf"
+const characteristicUuid = "00000003-710e-4a5b-8d75-3e5b444bc3cf";
+const trendInterval = 1000;
+const trendDuration = 30000;
 
 let bleCharacteristic;
-let values
+let trendTimer;
+let trendActive;
+let sourceState = { };
+let targetState = { };
+
+const zeroPad = (num, decimals, places) => String(num.toFixed(decimals)).padStart(places, '0')
+
+const stateHandlers = {
+    sliderHeartRate: async (value) => {
+        // Normal sinus rythm pediatric. bpm, 3 digits 010 to 360
+        await sendCommand(`NSRP=${zeroPad(value, 0, 3)}`);
+    },
+    sliderRespiratoryRate: async (value) => {
+        // Set respiration rate. bpm, 3 digits 010 to 150
+        await sendCommand(`RESPRATE=${zeroPad(value, 0, 3)}`);
+    },
+    sliderHeartSpO2: async (value) => {
+        // Sets SpO2 saturation percentage. Unsigned 3 digits: 000 to 100
+        await sendCommand(`SAT=${zeroPad(value, 0, 3)}`);
+    },
+    sliderBloodPressure1: async (value) => {
+        const lo = Math.round(value * 0.8);
+        // Set the NIBP dynamic pressure. Systolic pressure: unsigned 3 digits: 000 to 400. Diastolic pressure: unsigned 3 digits: 000 to 400
+        await sendCommand(`NIBPP=${zeroPad(value, 0, 3)},${zeroPad(lo, 0, 3)}`);
+        // Set an IBP channel to static pressure. Channel 1 or 2 followed by signed static pressure. 3 digits -010 to +300
+        await sendCommand(`IBPS=1,+${zeroPad(value, 0, 3)}`);
+
+    },
+    sliderBloodPressure2: async (value) => {
+        const hi = Math.round(value * 1.2);
+        // Set the NIBP dynamic pressure. Systolic pressure: unsigned 3 digits: 000 to 400. Diastolic pressure: unsigned 3 digits: 000 to 400
+        await sendCommand(`NIBPP=${zeroPad(hi, 0, 3)},${zeroPad(value, 0, 3)}`);
+        // Set an IBP channel to static pressure. Channel 1 or 2 followed by signed static pressure. 3 digits -010 to +300
+        await sendCommand(`IBPS=2,+${zeroPad(value, 0, 3)}`);
+
+    },
+    sliderTemp: async (value) => {
+        // Set the temparature. Degrees C, 3 digits w/dp: 30.0 to 42.0 [by 00.5]
+        await sendCommand(`TEMP=${zeroPad(value, 1, 3)}`);
+    }
+}
 
 async function connectBle() {
     const options = {
@@ -16,9 +58,94 @@ async function connectBle() {
     const service = await device.gatt.getPrimaryService(serviceUuid);
     bleCharacteristic = await service.getCharacteristic(characteristicUuid);
     console.log(`Connected to ${device.name}`);
+
+    targetState = sourceState;
+    activateState();
 }
 
-function setupSlider(name, minValue, maxValue, diffAxis, startValue, step, callback) {
+async function activateState() {
+    for (const sliderKey in targetState) {
+        if (sliderKey in stateHandlers) {
+            await stateHandlers[sliderKey](targetState[sliderKey]);
+        }
+    }
+    sourceState = { ...targetState };
+    targetState = { };
+
+    $('#activateButton').css('visibility', 'hidden');
+    $('#trendButton').css('visibility', 'hidden');
+
+    if (bleCharacteristic) {
+        for (sliderKey in sourceState) {
+            $(`#${sliderKey}`).slider('enable');
+        }
+    }
+}
+
+function openDialog(id) {
+    $(`#${id}`).css('visibility', 'visible');
+}
+
+function closeDialog(id) {
+    $(`#${id}`).css('visibility', 'hidden');
+}
+
+async function trendState() {
+    //openDialog('trendDialog');
+    const startTime = new Date().getTime();
+
+    trendActive = true;
+    for (sliderKey in sourceState) {
+        $(`#${sliderKey}`).slider('value', sourceState[sliderKey]);
+        $(`#${sliderKey}`).slider('disable');
+    }
+    $('#activateButton').css('visibility', 'hidden');
+    $('#trendButton').css('visibility', 'hidden');
+    $('#trendTimer').css('display', 'block');
+
+    if (trendTimer) {
+        clearInterval(trendTimer);
+        trendActive = false;
+    }
+
+    trendTimer = setInterval(() => {
+        const elapsedTime = new Date().getTime() - startTime;
+        const relTime = elapsedTime / trendDuration;
+        if (relTime >= 1) {
+            for (sliderKey in targetState) {
+                $(`#${sliderKey}`).slider('value', targetState[sliderKey]);
+            }
+            $('#trendTimer').css('display', 'none');
+            clearInterval(trendTimer);
+            trendActive = false;
+        } else {
+            for (sliderKey in targetState) {
+                const sourceValue = sourceState[sliderKey];
+                const targetValue = targetState[sliderKey];
+                const value = ((1 - relTime) * sourceValue) + (relTime * targetValue);
+                $(`#${sliderKey}`).slider('value', value);
+                stateHandlers[sliderKey](value);
+            }
+        }
+    }, trendInterval);
+}
+
+function updateTargetState() {
+    $('#activateButton').css('visibility', 'visible');
+    $('#trendButton').css('visibility', 'visible');
+}
+
+function setupSlider(name, minValue, maxValue, diffAxis, startValue, step = 1) {
+    sourceState[name] = startValue;
+
+    const refreshValue = (value) => {
+        if (step == 1) {
+            $(`#${name}Value`).html(value);
+        } else {
+            $(`#${name}Value`).html(value.toFixed(1));
+        }
+    }
+
     $(`#${name}`).slider({
         orientation: 'vertical',
         range: 'min',
@@ -26,22 +153,16 @@ function setupSlider(name, minValue, maxValue, diffAxis, startValue, step, callb
         max: maxValue,
         value: startValue,
         step: step,
-        slide: function(event, ui) {
-            if ($(this).slider("option", "step") == 1) {
-                $(`#${name}Value`).html(ui.value);
-            } else {
-                $(`#${name}Value`).html(ui.value.toFixed(1));
-            }
-            if (callback) {
-                callback(ui.value);
+        disabled: true,
+        change: (event, ui) => {
+            refreshValue(ui.value);
+            if (!trendActive) {
+                targetState[name] = ui.value;
+                updateTargetState();
             }
         }
     });
-    if (step == 1) {
-        $(`#${name}Value`).html(startValue);
-    } else {
-        $(`#${name}Value`).html(startValue.toFixed(1));
-    }
+    refreshValue(startValue);
 
     // Add the y-axis 
     $(`#${name}Axis`).append(`<div class="max-value">${maxValue} –</div>`);
@@ -58,132 +179,22 @@ async function sendCommand(command) {
     const encoder = new TextEncoder();
     try {
         await bleCharacteristic.writeValue(encoder.encode(command));
+        await new Promise((resolve) => setTimeout(resolve, 50));
     }
     catch { }
 }
 
-function addEventHandlerToSlider(name, eventFn) {
-    $(`#${name}`).on('mouseup mouseout', function() {
-        const value = $(`#${name}`).slider('option', 'value');
-        //eventFn(value);
-    });
-}
-
-const zeroPad = (num, decimals, places) => String(num.toFixed(decimals)).padStart(places, '0')
 
 $(function() {
     // Document is ready to be manipulated
 
-    const heartRateChanged = (value) => {
-         // Normal sinus rythm pediatric. bpm, 3 digits 010 to 360
-         sendCommand(`NSRP=${zeroPad(value, 0, 3)}`);
-    }
+    setupSlider('sliderHeartRate',       10, 240, 20, 70);
+    setupSlider('sliderRespiratoryRate', 10, 150, 10, 50);
+    setupSlider('sliderHeartSpO2',       0,  100, 10, 90);
+    setupSlider('sliderBloodPressure1',  0,  300, 30, 90);
+    setupSlider('sliderBloodPressure2',  0,  300, 30, 90);
+    setupSlider('sliderTemp',            30, 42,  1,  37);
 
-    const respiratoryRateChanged = (value) => {
-        // Set respiration rate. bpm, 3 digits 010 to 150
-        sendCommand(`RESPRATE=${zeroPad(value, 0, 3)}`);
-    }
-
-    const heartSpo2Changed = (value) => {
-        // Sets SpO2 saturation percentage. Unsigned 3 digits: 000 to 100.
-        sendCommand(`SAT=${zeroPad(value, 0, 3)}`);
-    }
-
-    const tempChanged = (value) => {
-        // Set the temparature. Degrees C, 3 digits w/dp: 30.0 to 42.0 [by 00.5]
-        sendCommand(`TEMP=${zeroPad(value, 1, 3)}`);
-    }
-
-    setupSlider('sliderHeartRate',       10, 360, 20, 70, 1, heartRateChanged);
-    setupSlider('sliderResipratoryRate', 10, 150, 10, 50, 1, respiratoryRateChanged);
-    setupSlider('sliderHeartSpO2',       0,  100, 10, 90, 1, heartSpo2Changed);
-    setupSlider('sliderBloodPressure1',  -10,  300, 30, 90, 1);
-    setupSlider('sliderBloodPressure2',  -10,  300, 30, 90, 1);
-    setupSlider('sliderTemp',            30, 42,  1,  37, 0.5, tempChanged);
+    targetState = sourceState;
+    activateState();
 });
-
-
-// –––––––––––––––––––––––––––––––––––––  MODALS  –––––––––––––––––––––––––––––––––––––
-
-function openModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "flex";
-}
-
-function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    var modal = document.getElementById("myModal");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}   
-
-
-function openModal(modalId, buttonContentId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "flex";
-
-    var closeModalButton = modal.querySelector('.close');
-    closeModalButton.onclick = function () {
-        closeModal(modalId);
-    };
-
-    // Attach event listeners for each option button in the modal
-    var optionButtons = modal.querySelectorAll('.option-buttons button');
-    optionButtons.forEach(function (button) {
-        button.onclick = function () {
-            var content = button.textContent;
-            updateButtonContent(buttonContentId, content);
-            closeModal(modalId);
-            selectOption(button);
-        };
-    });
-}
-
-function closeModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "none";
-}
-
-function updateButtonContent(buttonContentId, content) {
-    var buttonContent = document.getElementById(buttonContentId);
-    buttonContent.textContent = content;
-}
-
-function selectOption(button) {
-    console.log('Button clicked:', button);
-    // Rest of the code...
-        // Remove the 'selected' class from all buttons in the modal
-        var modalButtons = document.querySelectorAll('.button.box');
-        modalButtons.forEach(function (btn) {
-            btn.classList.remove('selected');
-        });
-    
-        // Add the 'selected' class to the clicked button
-        button.classList.add('selected');
-}
-
-// Live Mode Switch Code
-$(".checkitem").click(function(){
-    var item = $(".checkitem");
-    if (item.hasClass("itemactive")){
-      // It is activated. Deactivate it!
-      item.removeClass("itemactive");
-      //$(".customcheck").css("background-color", "var(--fhs-gray-gray-800)")
-      $(".customcheck").removeClass("customcheckActive");
-      $(".switchHolder").removeClass("switchHolderActive");
-      $("input[name='switch']").prop("checked", false);
-    } else {
-      // It's deactivated. Activate it!
-      item.addClass("itemactive");
-      //$(".customcheck").css("background-color", "var(--success-800)")
-      $(".customcheck").addClass("customcheckActive");
-      $(".switchHolder").addClass("switchHolderActive");
-      $("input[name='switch']").prop("checked", true);
-    }
-  });
